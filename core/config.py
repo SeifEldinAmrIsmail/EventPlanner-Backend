b5f90cd1c4f60e29ce1d4b36bca42e9e80b33ddf
Abdelrahman178 <123303649+Abdelrahman178@users.noreply.github.com>
2025-12-26 05:10:44 +0200

    Update MONGO_URI for authentication source

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:admin123@mongo:27017/eventplanner")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:admin123@mongo:27017/eventplanner?authSource=admin")
 DB_NAME = os.getenv("DB_NAME", "eventplanner")
 
 JWT_SECRET = os.getenv("JWT_SECRET", "dev_secret_change_me")
