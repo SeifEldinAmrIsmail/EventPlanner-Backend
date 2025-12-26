146720d3aa26db7f85cf5e1873772338f4d96300
Abdelrahman178 <123303649+Abdelrahman178@users.noreply.github.com>
2025-12-26 05:05:32 +0200

    Update MONGO_URI for production configuration

--- core/config.py
+++ core/config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+#MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:admin123@mongo:27017/eventplanner")
 DB_NAME = os.getenv("DB_NAME", "eventplanner")
 
 JWT_SECRET = os.getenv("JWT_SECRET", "dev_secret_change_me")
